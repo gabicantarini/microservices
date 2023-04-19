cd699ea808153808f7ebe4b984d2b5cdd937635d
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-04-19 22:46:32 +0100

    added migrations

--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -4,31 +4,61 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Add services to the container.
 var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
 
-builder.Services.AddDbContext<MySQLContext>(options => options.
-    UseMySql(connection, 
+
+builder.Services.AddDbContext<MySQLContext>(options => 
+    options.UseMySql(connection, 
         new MySqlServerVersion(
             new Version(8, 0, 25))));
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySqlContext>()
+    .AddDefaultTokenProviders();
+
+var builderIdentity = builder.Services.AddIdentityServer(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddAspNetIdentity<ApplicationUser>();*/
+
+//builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
+//builderIdentity.AddDeveloperSigningCredential();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+var scope = app.Services.CreateScope();
+
+//var dbInitializer = scope.ServiceProvider.GetService<IDbInitializer>();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
 }
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//app.UseIdentityServer();
 
 app.UseAuthorization();
 
-app.MapControllers();
+
+//dbInitializer.Initialize();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
