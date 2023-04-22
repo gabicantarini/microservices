de6bf9a9f71657232cda3860a28eba60a3692fce
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-04-22 22:23:55 +0100

    added product repository class

---
+++ GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<ProductVO> Create(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductVO>> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductVO> FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductVO> Update(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
