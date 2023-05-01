3bb840d8127b0cdcde660e1b1c46a08f53b56fb5
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-05-01 22:48:09 +0100

    added products

--- GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -10,6 +10,50 @@ namespace GeekShopping.ProductAPI.Model.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
     
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 1,
+                Name = "Mario Bross Mug",
+                Price = new decimal(69.90),
+                Description = "",
+                ImageUrl = "https://github.com/gabicantarini/microservices/blob/main/GeekShopping/ShoppingImages/10_milennium_falcon.jpg",
+                CategoryName = "Mug"
+
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                Name = "Milennium Falcon",
+                Price = new decimal(359.99),
+                Description = "Star Wars Mission Fleet Han Solo Nave Milennium Falcon",
+                ImageUrl = "https://github.com/gabicantarini/microservices/blob/main/GeekShopping/ShoppingImages/10_milennium_falcon.jpg",
+                CategoryName = "Action Figure"
+
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Gamer T-shirt",
+                Price = new decimal(69.90),
+                Description = "",
+                ImageUrl = "https://github.com/gabicantarini/microservices/blob/main/GeekShopping/ShoppingImages/10_milennium_falcon.jpg",
+                CategoryName = "T-Shirt"
+
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Coffee Benefits T-shirt",
+                Price = new decimal(69.90),
+                Description = "",
+                ImageUrl = "https://github.com/gabicantarini/microservices/blob/main/GeekShopping/ShoppingImages/10_milennium_falcon.jpg",
+                CategoryName = "T-Shirt"
+
+            });
+        }
     
     }
 }
