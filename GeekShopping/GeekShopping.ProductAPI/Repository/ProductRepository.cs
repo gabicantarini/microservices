2874347c16bf9ab9615f7afc8dffef0ab746455d
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-05-02 21:31:39 +0100

    added productvo findall

--- GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
+++ GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -4,11 +4,31 @@ using GeekShopping.ProductAPI.Model.Context;
 
 namespace GeekShopping.ProductAPI.Repository
 {
-    public class ProductRepository : MySqlConnector
+    public class ProductRepository : IProductRepository
     {
-        private readonly MySqlConnector _context;
+        private readonly MySqlContext _context;
         private IMapper _mapper;
+
+        public async Task<IEnumerable<ProductVO>> FindAll()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+            return _mapper.Map<List<ProductVO>>(products);
+            //busca a lista de products e retorna o que tiver lá
+        }
+
+        public Task<ProductVO> FindById(long id)
+        {
+            Product product = await _context.Products.Where(product => product.Id == id);
+            return _mapper.Map<List<ProductVO>>(product);
+            //busca a lista de products e retorna o que tiver lá
+        }
+
         public Task<ProductVO> Create(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductVO> Update(ProductVO vo)
         {
             throw new NotImplementedException();
         }
@@ -18,19 +38,6 @@ namespace GeekShopping.ProductAPI.Repository
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<ProductVO>> FindAll()
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task<ProductVO> FindById(long id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ProductVO> Update(ProductVO vo)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
