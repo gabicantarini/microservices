cb2ab6f30fd90efa61d4e06f6b5ccccdf7e391a3
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-05-05 12:11:54 +0100

    addedsuccess http response conditional

---
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtension.cs
@@ -0,0 +1,23 @@
+﻿namespace GeekShopping.Web.Utils;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public static class HttpClientExtension
+{
+    private static MediaTypeHeaderValue contentType
+= new MediaTypeHeaderValue("application/json");
+    public static async Task<T> ReadContentAs<T>(
+        this HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode) throw
+                 new ApplicationException(
+                     $"Something went wrong calling the API: " +
+                     $"{response.ReasonPhrase}");
+        var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        return JsonSerializer.Deserialize<T>(dataAsString,
+            new JsonSerializerOptions
+            { PropertyNameCaseInsensitive = true });
+    }
+}
