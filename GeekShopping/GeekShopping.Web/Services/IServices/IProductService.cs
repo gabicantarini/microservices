724af5719e391dc125b1593bdd07f3caeac411dd
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-05-05 16:38:23 +0100

    added find products

--- GeekShopping/GeekShopping.Web/Services/IServices/IProductService.cs
+++ GeekShopping/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -1,16 +1,15 @@
-﻿namespace GeekShopping.Web.Services.IServices;
-using GeekShopping.Web.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
 
 {
     public interface IProductService
     {
-        Task<IEnumerable<ProductModel>> FindAllProducts();
-        Task<ProductModel> FindProductById(long id);
-        Task<ProductModel> CreateProduct(ProductModel model);
-        Task<ProductModel> UpdateProduct(ProductModel model);
-        Task<bool> DeleteProductById(long id);
+        Task<IEnumerable<ProductModel>> FindAllProducts(string token);
+        Task<ProductModel> FindProductById(long id, string token);
+        Task<ProductModel> CreateProduct(ProductModel model, string token);
+        Task<ProductModel> UpdateProduct(ProductModel model, string token);
+        Task<bool> DeleteProductById(long id, string token);
 
     }
 }
---
+++ GeekShopping/GeekShopping.Web/Services/ProductService.cs
@@ -0,0 +1,36 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Utils;
+using System.Net.Http.Headers;
+
+namespace GeekShopping.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _client;
+        public const string BasePath = "api/v1/product";
+
+        public ProductService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<ProductModel>> FindAllProducts(string token)
+        {
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.GetAsync(BasePath);
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindProductById(long id, string token)
+        {
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+
+
+
+    }
+}
