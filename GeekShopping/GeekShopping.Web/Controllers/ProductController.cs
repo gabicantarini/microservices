07ae71668c2f480dd291e1bb8cec2d36bba4366a
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-05-09 23:17:30 +0100

    added updated

--- GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
@@ -1,5 +1,13 @@
 ﻿using GeekShopping.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using GeekShopping.Web.Models;
+using GeekShopping.Web.Utils;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace GeekShopping.Web.Controllers
 {
@@ -30,6 +38,28 @@ namespace GeekShopping.Web.Controllers
             {
                 var token = await HttpContext.GetTokenAsync("access_token");
                 var response = await _productService.CreateProduct(model, token);
+                if (response != null) return RedirectToAction(
+                     nameof(ProductIndex));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductUpdate(int id)
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var model = await _productService.FindProductById(id, token);
+            if (model != null) return View(model);
+            return NotFound();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var token = await HttpContext.GetTokenAsync("access_token");
+                var response = await _productService.UpdateProduct(model, token);
                 if (response != null) return RedirectToAction(
                      nameof(ProductIndex));
             }
