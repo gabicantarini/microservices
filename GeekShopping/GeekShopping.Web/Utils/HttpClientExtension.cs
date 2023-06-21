4270043502fae4a9d0c20a8dcd9515349314116b
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-06-21 11:20:17 +0100

    added role

--- GeekShopping/GeekShopping.Web/Utils/HttpClientExtension.cs
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtension.cs
@@ -13,7 +13,7 @@ namespace GeekShopping.Web.Utils
         public static async Task<T> ReadContentAs<T>(
             this HttpResponseMessage response)
         {
-            if (!response.IsSuccessStatusCode) throw
+
                      new ApplicationException(
                          $"Something went wrong calling the API: " +
                          $"{response.ReasonPhrase}");
